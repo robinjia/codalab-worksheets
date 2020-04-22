bc118136274a061d9b5d0b420b5e49ba44e692c4
Tony Lee <tonyh.lee@yahoo.com>
2020-04-21 22:23:39 -0700

    default port

--- test_runner.py
+++ test_runner.py
@@ -52,7 +52,7 @@ class TestRunner(object):
                         TestRunner._CODALAB_SERVICE_EXECUTABLE,
                         'start',
                         '--instance-name %s' % name,
-                        '--rest-port %s' % rest_port,
+                        #'--rest-port %s' % rest_port,
                         #'--http-port %s' % http_port,
                         #'--mysql-port %s' % mysql_port,
                         '--version %s' % version,
