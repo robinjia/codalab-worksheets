955ae8023c6991ac427dba151a3932b0c20998a7
Tony Lee <tonyh.lee@yahoo.com>
2020-04-23 10:59:04 -0700

    add mysql too

--- test_runner.py
+++ test_runner.py
@@ -67,7 +67,7 @@ class TestRunner(object):
                         '--rest-port %s' % rest_port,
                         # '--http-port %s' % http_port,
                         '--version %s' % version,
-                        '--services init rest-server bundle-manager',
+                        '--services init rest-server bundle-manager mysql',
                         # '--services rest-server',
                     ]
                 ),
