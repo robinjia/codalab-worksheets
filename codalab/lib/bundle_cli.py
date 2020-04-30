8b1ddac857b2a4e61545d93d4c6b5aad6f8cfac5
Jing Ge <stanford@Stanfords-MacBook-Pro.local>
2020-04-29 17:47:38 -0700

    Fix wadd empty bundles across intances

--- codalab/lib/bundle_cli.py
+++ codalab/lib/bundle_cli.py
@@ -1441,8 +1441,14 @@ class BundleCLI(object):
                 file=self.stdout,
             )
             return
-
         print("Copying %s..." % source_desc, file=self.stdout)
+
+        # If bundle contents don't exist, finish after just copying metadata
+        try:
+            target_info = source_client.fetch_contents_info(BundleTarget(source_bundle_uuid, ''))
+            has_contents = True
+        except NotFoundError:
+            has_contents = False
 
         # Create the bundle, copying over metadata from the source bundle
         dest_bundle = dest_client.create(
@@ -1452,13 +1458,10 @@ class BundleCLI(object):
                 'worksheet': dest_worksheet_uuid,
                 'detached': not add_to_worksheet,
                 'wait_for_upload': True,
+                'state_on_success': None if has_contents else source_info['state'],
             },
         )
-
-        # If bundle contents don't exist, finish after just copying metadata
-        try:
-            target_info = source_client.fetch_contents_info(BundleTarget(source_bundle_uuid, ''))
-        except NotFoundError:
+        if not has_contents:
             return
 
         # Collect information about how server should unpack
@@ -1468,14 +1471,6 @@ class BundleCLI(object):
             unpack = True
         else:
             unpack = False
-
-        # Bundles stuck in non-final states such as 'running' should not keep
-        # that state at the destination server, and should instead just fallback
-        # to 'failed'
-        if source_info['state'] == State.READY:
-            source_state = State.READY
-        else:
-            source_state = State.FAILED
 
         # Send file over
         progress = FileTransferProgress('Copied ', f=self.stderr)
@@ -1488,7 +1483,7 @@ class BundleCLI(object):
                     'filename': filename,
                     'unpack': unpack,
                     'simplify': False,  # retain original bundle verbatim
-                    'state_on_success': source_state,  # copy bundle state
+                    'state_on_success': source_info['state'],  # copy bundle state
                     'finalize_on_success': True,
                 },
                 progress_callback=progress.update,
